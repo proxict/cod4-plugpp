98c04d1fbe30dfe20ebf7a144023db8199965e03
United Promod <admin@united-promod.com>
2023-08-06 16:47:58 +0200

    Derive plugpp::Exception from std::runtime_error

--- include/cod4-plugpp/Exception.hpp
+++ include/cod4-plugpp/Exception.hpp
@@ -1,41 +1,27 @@
 #ifndef COD4_PLUGPP_INCLUDE_COD4_PLUGPP_EXCEPTION_HPP_
 #define COD4_PLUGPP_INCLUDE_COD4_PLUGPP_EXCEPTION_HPP_
 
+#include <cod4-plugpp/utils/stringUtils.hpp>
+
 #include <sstream>
+#include <stdexcept>
 #include <string>
 
 namespace plugpp {
 
-class Exception {
+class Exception : public std::runtime_error {
 public:
     template <typename... TArgs>
     explicit Exception(const std::string& message, TArgs&&... args)
-        : mMessage(stringify(message, std::forward<TArgs>(args)...)) {}
+        : std::runtime_error(plugpp::concatenate(message, std::forward<TArgs>(args)...)) {}
 
     virtual ~Exception() noexcept = default;
 
-    virtual const std::string& what() const noexcept { return mMessage; }
-
-    Exception(Exception&&) = default;
-
-private:
-    template <typename T>
-    std::string stringify(const T& arg) {
-        std::ostringstream ss;
-        ss << arg;
-        return ss.str();
-    }
-
-    template <typename T, typename... Args>
-    std::string stringify(const T& first, Args&&... args) {
-        return stringify(first) + stringify(std::forward<Args>(args)...);
-    }
+    Exception(Exception&&) noexcept = default;
 
     Exception(const Exception&) = delete;
     Exception& operator=(const Exception&) = delete;
     Exception& operator=(Exception&&) = delete;
-
-    const std::string mMessage;
 };
 
 } // namespace plugpp
