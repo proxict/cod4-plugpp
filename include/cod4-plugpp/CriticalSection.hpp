8bbb548104463e03516125216e732fdb80aa19f5
United Promod <admin@united-promod.com>
2023-08-07 02:11:19 +0200

    Added CriticalSection doxygen

--- include/cod4-plugpp/CriticalSection.hpp
+++ include/cod4-plugpp/CriticalSection.hpp
@@ -5,12 +5,15 @@
 
 namespace plugpp {
 
+/// A critical section wrapper that can be used with std::lock_guard and alike.
 class CriticalSection final {
 public:
     explicit CriticalSection() = default;
 
+    /// Enters the plugin's critical section
     void lock() noexcept { Plugin_EnterCriticalSection(); }
 
+    /// Leaves the plugin's critical section
     void unlock() noexcept { Plugin_LeaveCriticalSection(); }
 
     CriticalSection& operator=(const CriticalSection&) = delete;
